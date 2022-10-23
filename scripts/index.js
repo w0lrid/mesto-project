b8db8485a33411bbf4597f185a20c38fd4cad8bf
Dmitry Kushnerev <dmitry.k@profactory.io>
2022-10-23 10:32:02 +0300

    feat: update like-button structure and script

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
+const likeButtons = document.querySelectorAll('.card__like-button');
 const name = document.querySelector('.profile__name').innerHTML;
 const description = document.querySelector('.profile__subtitle').innerHTML;
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup')
+
+likeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('card__like-button_active')) {
+      button.classList.remove('card__like-button_active');
+    } else {
+      button.classList.add('card__like-button_active');
+    }
+  })
+})
 
 editButton.addEventListener('click', () => {
   const nameInput = document.getElementById('name')
