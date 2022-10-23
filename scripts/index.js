8dcfebde2804fefb1e7a036b7baaa768f45adefc
Dmitry Kushnerev <dmitry.k@profactory.io>
2022-10-23 12:23:19 +0300

    refactor: common layout and styles improvements

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const likeButtons = document.querySelectorAll('.card__like-button');
-const name = document.querySelector('.profile__name').innerHTML;
-const description = document.querySelector('.profile__subtitle').innerHTML;
+const profileName = document.querySelector('.profile__name').textContent;
+const profileDescription = document.querySelector('.profile__subtitle').textContent;
 const editButton = document.querySelector('.profile__edit-button');
+const saveButton = document.querySelector('.popup__button');
 const closeButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup')
 
@@ -17,13 +18,19 @@ likeButtons.forEach((button) => {
 
 editButton.addEventListener('click', () => {
   const nameInput = document.getElementById('name')
-  nameInput.value = name
+  nameInput.value = profileName
+
   const descriptionInput = document.getElementById('description')
-  descriptionInput.value = description
+  descriptionInput.value = profileDescription
+
   popup.classList.add('popup_active')
 });
 
+saveButton.addEventListener('click', () => {
+  popup.classList.remove('popup_active')
+})
+
+
 closeButton.addEventListener('click', () => {
   popup.classList.remove('popup_active')
-  console.log('close')
 })
