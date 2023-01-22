a4c265c0d6f55902897ca5e220ff4d0f519f73ac
Dmitrii Kushnerev <w0lridem@gmail.com>
2023-01-22 22:49:19 +0300

    feat: set base form validation functionality; add validation to the `add image` form

--- src/index.js
+++ src/index.js
@@ -29,6 +29,8 @@ import initialCards from "../config/cards.config.js";
 
 // templates
 const cardTemplate = document.getElementById("card-template").content;
+const forms = document.forms;
+
 
 // common elements
 const cardsGallery = document.querySelector(".cards-gallery");
@@ -151,3 +153,44 @@ function handleNewCardSubmit() {
 function deleteCard() {
   event.target.closest("article").remove();
 }
+
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+}
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+}
+
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement);
+    })
+  })
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach(formElement => {
+    console.log(formElement)
+    setEventListeners(formElement);
+  })
+}
+
+enableValidation()
